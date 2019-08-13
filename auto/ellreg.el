417f31b264fc75de978c680cb97f41e2e2c707d4
Dylan Cant <dcant1000@gmail.com>
2019-08-12 17:58:24 -0700

    talk ellreg

--- auto/ellreg.el
+++ auto/ellreg.el
@@ -1,13 +1,18 @@
 (TeX-add-style-hook
  "ellreg"
  (lambda ()
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "amsart"
+    "amsart10"
+    "preamble")
    (LaTeX-add-labels
     "claim:20"
     "claim:21"
---
+++ auto/talk-ellreg.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook
+ "talk-ellreg"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "amsart"
+    "amsart10"
+    "preamble"))
+ :latex)
+
